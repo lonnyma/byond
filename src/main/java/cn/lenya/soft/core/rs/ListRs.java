2c38132aac457a134c58ce17cfa48fcc3937d368
maqinglong <mql@byond.cn>
2018-09-08 21:39:54 +0800

    improve:改善包结构

--- src/main/java/cn/lenya/soft/core/rs/ListRs.java
+++ src/main/java/cn/lenya/soft/core/rs/ListRs.java
@@ -1,7 +1,7 @@
 /* Copyright (c) 2018 白羊人工智能在线技术. All rights reserved.
  * http://www.byond.cn
  */
-package cn.lenya.soft.common.rs;
+package cn.lenya.soft.core.rs;
 
 import java.util.List;
 
--- src/main/java/cn/lenya/soft/core/rs/ObjectRs.java
+++ src/main/java/cn/lenya/soft/core/rs/ObjectRs.java
@@ -1,7 +1,7 @@
 /* Copyright (c) 2018 白羊人工智能在线技术. All rights reserved.
  * http://www.byond.cn
  */
-package cn.lenya.soft.common.rs;
+package cn.lenya.soft.core.rs;
 
 public class ObjectRs<T> extends BaseRs {
 	private T object = null;
